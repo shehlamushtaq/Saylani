c846be717146a2b0f153377d9ac8ad681b4bde8a
shehlamushtaq <shehla.asim.78@gmail.com>
2020-11-09 12:46:46 +0500

    Dom and events

---
+++ practice.js
@@ -0,0 +1,35 @@
+
+// function humpty(){
+
+// var d = document.getElementById("humpty");
+
+//  var pCounter = 0;
+//  for (var i = 0; i < d.childNodes.length; i++) {
+//  if (d.childNodes[i].nodeType === 1 ) {
+//  pCounter++;
+//  }
+//  if (pCounter === 2) {
+//  d.childNodes[i].innerHTML = "All his men.";
+//  break;
+//  }
+//  }
+// }
+
+// function toDo() {
+//     // var task = prompt('Work to do')
+
+//     var x = document.getElementById('works')
+
+//     var li = document.createElement('li')
+//     var textLi = document.createTextNode(x.value) 
+//     li.appendChild(textLi)
+
+//     var ul = document.getElementById('ul')
+//     var newTask = ul.appendChild(li)
+//     x.value = ' '
+
+// }
+
+function name(params) {
+    
+}
